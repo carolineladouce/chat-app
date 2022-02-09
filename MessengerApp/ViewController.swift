004acb41957368fa9d5e4517afa5aa8c77f9e6ce
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-02-09 14:05:43 -0500

    Initial Commit

---
+++ MessengerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessengerApp
+//
+//  Created by Caroline LaDouce on 2/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
