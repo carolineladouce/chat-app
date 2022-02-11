1e634ba7e4091d6c30757e24ddb66df275eeef39
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-02-10 23:53:31 -0500

    Added user input field named emailField Created scrollView to contain imageView and emailField

--- MessengerApp/Controllers/Login/LoginViewController.swift
+++ MessengerApp/Controllers/Login/LoginViewController.swift
@@ -9,14 +9,35 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
+    
     private let imageView: UIImageView = {
-      let imageView = UIImageView()
-//        imageView.image = UIImage(named: "logo")
+        let imageView = UIImageView()
+        //        imageView.image = UIImage(named: "logo")
         imageView.image = "😎".emojiToImage()
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
-
+    
+    
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Email Address..."
+        
+        return field
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,7 +59,9 @@ class LoginViewController: UIViewController {
                                                             action: #selector(didTapRegister))
         
         // Add subviews
-        view.addSubview(imageView)
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
         
         
     }
@@ -46,11 +69,16 @@ class LoginViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        let size = view.width / 3
-        imageView.frame = CGRect(x: (view.width - size) / 2,
+        scrollView.frame = view.bounds
+        let size = scrollView.width / 3
+        imageView.frame = CGRect(x: (scrollView.width - size) / 2,
                                  y: 20,
                                  width: size,
                                  height: size)
+        emailField.frame = CGRect(x: 30,
+                                  y: imageView.bottom + 10,
+                                  width: scrollView.width - 60,
+                                  height: 52)
     }
     
     
