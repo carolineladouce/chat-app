26025b1ac388125c9c26aedc1f37c501c43ddb8e
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-03-09 22:43:55 -0500

    Setup template code for the profileTableView in ProfileViewController profileTableView currently displays sampleData

--- MessengerApp/Controllers/ProfileViewController.swift
+++ MessengerApp/Controllers/ProfileViewController.swift
@@ -7,31 +7,57 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let profileTableView = UITableView()
     var safeArea: UILayoutGuide!
+    
+    var sampleData = ["Test", "TestTest", "TestTestTest"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        view.addSubview(profileTableView)
+        profileTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        profileTableView.delegate = self
+        profileTableView.dataSource = self
         
-        safeArea = view.layoutMarginsGuide
-        
-        setupTableView()
     }
     
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        profileTableView.frame = view.bounds
+    }
+    
     func setupTableView() {
-        view.addSubview(profileTableView)
-        
         profileTableView.translatesAutoresizingMaskIntoConstraints = false
         profileTableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
         profileTableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         profileTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         profileTableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        
     }
     
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sampleData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = sampleData[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print("Cell tapped")
+    }
+    
+    
 }
+
+
+
+
