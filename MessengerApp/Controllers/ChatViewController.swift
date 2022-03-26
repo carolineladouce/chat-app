2f7c12c7e8493a50c57f781fa81789e2dad6caf9
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-03-26 19:33:37 -0400

    Setup ChatViewController basics for displaying messages

--- MessengerApp/Controllers/ChatViewController.swift
+++ MessengerApp/Controllers/ChatViewController.swift
@@ -6,14 +6,71 @@
 //
 
 import UIKit
+import MessageKit
 
-class ChatViewController: UIViewController {
+struct Message: MessageType {
+    var sender: SenderType
+    
+    var messageId: String
+    
+    var sentDate: Date
+    
+    var kind: MessageKind
+}
 
+
+struct Sender: SenderType {
+    var photoURL: String
+    
+    var senderId: String
+    
+    var displayName: String
+}
+
+class ChatViewController: MessagesViewController {
+    
+    private var messages = [Message]()
+    
+    private let selfSender = Sender(photoURL: "",
+                                    senderId: "1",
+                                    displayName: "Sample Name")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        messages.append(Message(sender: selfSender,
+                                messageId: "1",
+                                sentDate: Date(),
+                                kind: .text("Hello World message")))
+        
+        messages.append(Message(sender: selfSender,
+                                messageId: "1",
+                                sentDate: Date(),
+                                kind: .text("Hello World message. Hello World message. Hello World message.")))
+        
         view.backgroundColor = .purple
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
     }
     
+    
+}
 
+
+extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    func currentSender() -> SenderType {
+        return selfSender
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+    
+    
 }
