2d21c30ed06a9e703eec30ce4a7ec288fd89d988
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-03-25 20:32:07 -0400

    Created ChatViewController

---
+++ MessengerApp/Controllers/ChatViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChatViewController.swift
+//  MessengerApp
+//
+//  Created by Caroline LaDouce on 3/25/22.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
