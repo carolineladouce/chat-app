401f2cec038624c1b1d0084b8664b1ff8a4cab85
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-04-01 08:39:22 -0400

    DatabaseManager updated to add new user "name" and "email" to Firebase database so that user email, first name, and last name are stored/listed in an organized manner

--- MessengerApp/Resources/DatabaseManager.swift
+++ MessengerApp/Resources/DatabaseManager.swift
@@ -46,8 +46,10 @@ extension DatabaseManager {
         })
     }
     
+    
     /// Inserts new user to database
     public func insertUser(with user: ChatAppUser, completion: @escaping (Bool) -> Void) {
+        // When we insert a new user, we make the root entry:
         database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName
@@ -57,6 +59,85 @@ extension DatabaseManager {
                 completion(false)
                 return
             }
+            
+            /*
+             
+             We want to create a users array.
+             We will store multiple entries for each user
+             which will contain their "name" and "safe_email"
+             Every time we add a new user, we will create a new entry
+             This whole array of users will have one root child pointer
+             called "users"
+             The reason we are doing this is because
+             when the user tries to start a new conversation we can pull out
+             all of these users with one request, which will save database costs
+             and is considered more "clean"
+             
+             users => [
+             [
+             "name":
+             "safe_email":
+             ],
+             [
+             "name":
+             "safe_email":
+             ]
+             ]
+             
+             */
+            
+            
+            /*
+             Next, we want to add the entry info we want to add it to our array of users.
+             We need to check if the collection exists.
+             If it doesn't, we need to create the collection.
+             If it does, then we can append the entry
+             Once the append is done, we need to call the completion because
+             the completion for the whole func of insertUser is NOT finished until
+             both of the operations below succeed.
+             */
+            self.database.child("users").observeSingleEvent(of: .value, with: { snapshot in
+                /* We are setting usersCollection to var rather than let because
+                 var makes usersCollection mutable so that we can append more contents into
+                 */
+                if var usersCollection = snapshot.value as? [[String: String]] {
+                    // Append to users array
+                    let newElement = [
+                        "name": user.firstName + " " + user.lastName,
+                        "email": user.safeEmail
+                    ]
+                    usersCollection.append(newElement)
+                    
+                    self.database.child("users").setValue(usersCollection, withCompletionBlock: { error, _ in
+                        guard error == nil else {
+                            completion(false)
+                            return
+                        }
+                        
+                        completion(true)
+                    })
+                    
+                    
+                } else {
+                    // Create the that users array
+                    let newCollection: [[String: String]] = [
+                        [
+                            "name": user.firstName + " " + user.lastName,
+                            "email": user.safeEmail
+                        ]
+                    ] // Close array
+                    
+                    self.database.child("users").setValue(newCollection, withCompletionBlock: { error, _ in
+                        guard error == nil else {
+                            completion(false)
+                            return
+                        }
+                        
+                        completion(true)
+                    })
+                }
+            })
+            
             completion(true)
         })
     }
