a3a4430c499187a29994dee363f75d246c3f4a75
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-04-07 13:58:55 -0400

    Added file ConversationTableViewCell to project

---
+++ MessengerApp/Views/ConversationTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ConversationTableViewCell.swift
+//  MessengerApp
+//
+//  Created by Caroline LaDouce on 4/7/22.
+//
+
+import UIKit
+
+class ConversationTableViewCell: UITableViewCell {
+
+
+
+}
